b19c1389fa6b7aafcdb40d7feb3ab406a9798224
kahnuk <toonein@protonmail.com>
2020-06-03 18:42:47 -0600

    More verification stuff

--- cogs/staff_commands.py
+++ cogs/staff_commands.py
@@ -19,6 +19,7 @@ class staff_commands(commands.Cog):
         description = "ID Verification"
     )
     async def verify(self, ctx, user: discord.Member):
+        print(verification_users)
         user_roles = user.roles[1:]
         if not str(user.id) in verification_users:
             verification_users[str(user.id)] = user_roles
@@ -26,17 +27,35 @@ class staff_commands(commands.Cog):
             await user.remove_roles(i)
         verification = discord.utils.get(ctx.guild.roles, id = 476377701154947082)
         await user.add_roles(verification)
+        embed = discord.Embed(
+            title = "Success!",
+            description = f"User {user.mention} has been put in verification.",
+            colour = 0x7289da
+        )
+        embed.set_thumbnail(url = user.avatar_url)
+        await ctx.send(embed = embed)
 
     @commands.has_role(253619793691803658)
     @commands.command(
         name = 'verified'
     )
-    async def verified(self, ctx, user: discord.Member):
+    async def verified(self, ctx, user: discord.Member, *reason: str):
         verification = discord.utils.get(ctx.guild.roles, id = 476377701154947082)
+        if reason:
+            reason_str = ": " + " ".join(reason)
+        else:
+            reason_str = ""
         if str(user.id) in verification_users:
             for i in verification_users[str(user.id)]:
                 await user.add_roles(i)
         await user.remove_roles(verification)
-
+        embed = discord.Embed(
+            title = "Verified User",
+            description = f"User {user.mention} has been verified by {ctx.author.mention}{reason_str}",
+            colour = 0x7289da
+        )
+        embed.set_thumbnail(url = user.avatar_url)
+        verified_channel = discord.utils.get(ctx.guild.text_channels, id = int(476400912307453962))
+        await verified_channel.send(embed = embed)
 def setup(bot):
     bot.add_cog(staff_commands(bot))
