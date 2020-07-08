b80bce21f45fcc813d223c6a0acc7fcdea0eac93
kahnuk <toonein@protonmail.com>
2020-07-08 14:31:19 -0600

    Fixed guild specific rule commands... again

--- cogs/static_embeds.py
+++ cogs/static_embeds.py
@@ -6,6 +6,10 @@ import random
 from datetime import datetime
 from discord.ext import commands
 
+def is_in_guild(guild_id):
+        async def predicate(ctx):
+            return ctx.guild and ctx.guild.id == guild_id
+        return commands.check(predicate)
 
 commands_data = {}
 
