a61b971916b054aa407967909316499d3a1f2082
kahnuk <62972737+kahnuk@users.noreply.github.com>
2021-05-29 15:19:31 -0700

    Update utility_commands.py

--- cogs/utility_commands.py
+++ cogs/utility_commands.py
@@ -132,9 +132,9 @@ class utility_commands(commands.Cog):
             new_position += 1
             print(new_position)
             if not discord.utils.get(ctx.guild.roles, name = str(ctx.author.id)):
-                new_role = await ctx.guild.create_role(name = str(ctx.author.id), colour = role_colour)
-                new_role_created = await discord.utils.get(ctx.guild.roles, name = str(ctx.author.id))
-                await new_role_created.edit(colour = role_colour, position = new_position)
+                await ctx.guild.create_role(name = str(ctx.author.id), colour = role_colour)
+                new_role = discord.utils.get(ctx.guild.roles, name = str(ctx.author.id))
+                await new_role.edit(colour = role_colour, position = new_position)
                 await ctx.author.add_roles(new_role)
             else:
                 new_role = await discord.utils \
